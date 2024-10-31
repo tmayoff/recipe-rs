b440b2dfe2d4b2dce2bf9afa4434f2b4b358cce1
Tyler Mayoff <tyler@tylermayoff.com>
2024-10-31 13:38:19 -0400

    Coverage deps

--- flake.nix
+++ flake.nix
@@ -18,38 +18,13 @@
       system: let
         overlays = [
           (import rust-overlay)
-          (final: prev: {
-            wasm-pack = prev.rustPlatform.buildRustPackage rec {
-              pname = "wasm-pack";
-              version = "0.13.0";
-
-              src = prev.fetchFromGitHub {
-                owner = "rustwasm";
-                repo = "wasm-pack";
-                rev = "refs/tags/v${version}";
-                hash = "sha256-NEujk4ZPQ2xHWBCVjBCD7H6f58P4KrwCNoDHKa0d5JE=";
-              };
-
-              cargoHash = "sha256-pFKGQcWW1/GaIIWMyWBzts4w1hMu27hTG/uUMjkfDMo=";
-              nativeBuildInputs = with prev; [cmake];
-
-              buildInputs = prev.lib.optional prev.stdenv.isDarwin prev.darwin.apple_sdk.frameworks.Security;
-
-              # Most tests rely on external resources and build artifacts.
-              # Disabling check here to work with build sandboxing.
-              doCheck = false;
-
-              propagatedBuildInputs = with pkgs; [nodejs_22];
-            };
-          })
         ];
         pkgs = (import nixpkgs) {
           inherit system overlays;
         };
 
         rust = pkgs.rust-bin.stable.latest.default;
-      in rec {
-        # For `nix develop`:
+      in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             pkg-config
@@ -63,6 +38,8 @@
             })
             rust-analyzer
             wasm-pack
+
+            cargo-tarpaulin
           ];
         };
       }
