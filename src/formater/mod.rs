0bcd708cea08970b6ba3f62bc628eddfa188377d
Tyler Mayoff <tyler@tylermayoff.com>
2024-10-31 14:47:52 -0400

    Added directions to test

--- src/formater/mod.rs
+++ src/formater/mod.rs
@@ -36,24 +36,40 @@ mod tests {
     fn recipe_md() {
         let recipe = Recipe {
             name: "Test Recipe".to_string(),
-            ingredients: vec![Ingredient {
-                name: "flour".to_string(),
-                amounts: vec![Measure {
-                    unit: "cups".to_string(),
-                    upper_value: None,
-                    value: 1.5,
-                }],
-                modifier: None,
-            }],
-            directions: vec![],
+            ingredients: vec![
+                Ingredient {
+                    name: "flour".to_string(),
+                    amounts: vec![Measure {
+                        unit: "cups".to_string(),
+                        upper_value: None,
+                        value: 1.5,
+                    }],
+                    modifier: None,
+                },
+                Ingredient {
+                    name: "egg".to_string(),
+                    amounts: vec![Measure {
+                        unit: "".to_string(),
+                        upper_value: None,
+                        value: 1.0,
+                    }],
+                    modifier: None,
+                },
+            ],
+            directions: vec!["Crack an egg".to_string(), "Add flour".to_string()],
         };
 
         let expected = r##"# Ingredients
 ---
 - 1.5 cups flour
+- 1  egg
 ---
 
 # Directions
+1. Crack an egg
+
+2. Add flour
+
 "##;
 
         let got = super::recipe_md(&recipe);
